83010fc5560baf052262e59620a0fcf9e1137395
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-06-01 16:49:27 -0400

    Initial Commit

---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        // Declare Fields
+        private string hunger;
+        private string thirst;
+        private string sleep;
+
+        // Declare Properties
+        public string Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public string Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public string Sleep
+        {
+            get { return this.sleep; }
+            set { this.sleep = value; }
+        }
+
+        VirtualPet()
+        {
+            // Default Constructor
+        }
+
+        VirtualPet(int hunger, int thirst, int sleep)
+        {
+            // Loaded Constructor
+        }
+
+        public void Eat()
+        {
+
+        }
+
+        public void Drink()
+        {
+
+        }
+
+        public void TakeNap()
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+
+
+
+    }
+}
